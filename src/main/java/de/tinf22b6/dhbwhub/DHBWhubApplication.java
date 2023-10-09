107195b62c250fd65afb0a808bef878e4154cd57
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-10-09 16:06:29 +0200

    Project setup (Spring and React)

---
+++ src/main/java/de/tinf22b6/dhbwhub/DHBWhubApplication.java
@@ -0,0 +1,13 @@
+package de.tinf22b6.dhbwhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DHBWhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DHBWhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tinf22b6/dhbwhub/DHBWhubApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tinf22b6.dhbwhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DHBWhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
