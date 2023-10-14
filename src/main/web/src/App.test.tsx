7bdb8302361f5348f9fa44a659ef265c60f8795f
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-10-14 11:43:36 +0200

    Changed App.test.tsx

--- src/main/web/src/App.test.tsx
+++ src/main/web/src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the App component with Home route', () => {
+  const { getByText } = render(<App />);
+
+  // Überprüfen Sie, ob der Text oder ein anderes Element aus Ihrer Home-Komponente vorhanden ist.
+  expect(getByText('DHBW')).toBeInTheDocument();
+  expect(getByText('Hub')).toBeInTheDocument();
 });
