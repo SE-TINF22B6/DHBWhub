53d7b1c5b4e349a1fb43a075889b5f0c294e44ba
denniskp <122602355+denniskp@users.noreply.github.com>
2023-10-28 14:02:40 +0200

    feat: add proposal classes

---
+++ src/main/java/de/tinf22b6/dhbwhub/proposal/AdministratorProposal.java
@@ -0,0 +1,14 @@
+package de.tinf22b6.dhbwhub.proposal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdministratorProposal {
+    private AccountProposal account;
+}
