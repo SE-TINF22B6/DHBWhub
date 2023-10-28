e6122f22e8b602f8addf19a816d5a21d1f7deff7
denniskp <122602355+denniskp@users.noreply.github.com>
2023-10-28 14:40:03 +0200

    feat: add repository classes

---
+++ src/main/java/de/tinf22b6/dhbwhub/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package de.tinf22b6.dhbwhub.repository;
+
+import de.tinf22b6.dhbwhub.model.User;
+import de.tinf22b6.dhbwhub.repository.interfaces.SpringUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserRepository {
+    private final SpringUserRepository repository;
+
+    public UserRepository(@Autowired SpringUserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User find(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/de/tinf22b6/dhbwhub/repository/interfaces/SpringFacultyRepository.java
@@ -0,0 +1,6 @@
+package de.tinf22b6.dhbwhub.repository.interfaces;
+
+import de.tinf22b6.dhbwhub.model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpringFacultyRepository extends JpaRepository<Faculty, Long> {}
