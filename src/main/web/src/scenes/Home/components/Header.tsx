841fe5ab7d9081d73b7d06b2f86fc359772cb933
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-10-29 12:04:38 +0100

    Created Header component and stylesheet

--- src/main/web/src/scenes/Home/components/Header.tsx
+++ src/main/web/src/scenes/Home/components/Header.tsx
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SearchBar} from "./SearchBar";
+import { Link } from 'react-router-dom';
 import "./Header.css";
 import {Logo} from "./Logo";
 
 export const Header = () => {
 
+  const [newNotifications, setNewNotifications] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(true);
+
   return (
       <div className="header">
         <div className="header-content">
-          <Logo/>
-          <div className="home-button"></div>
-          <div className="friends-button">
-            <div className="friends-picture"/>
-          </div>
-          <div className="calendar-button">
-            <div className="calendar-icon"/>
-          </div>
-          <div className="searchbar">
-            <SearchBar/>
-          </div>
-          <div className="notifications-button"/>
-          <div className="profile-component">
-            <div className="login">
-              <div className="log-in-wrapper">
-                <div className="log-in-text">LOGIN</div>
-              </div>
+          <Link to="/" className="home-button">
+            <Logo/>
+          </Link>
+          <Link to="/" className="home-button">
+            <div className="home-background"></div>
+          </Link>
+          <Link to="/friends" className="friends-button">
+            <div className="friends-background">
+              <div className="friends-picture" />
             </div>
-            <div className="sign-up">
-              <div className="sign-up-wrapper">
-                <div className="sign-up-text">SIGN UP</div>
-              </div>
+          </Link>
+          <Link to="/calendar" className="calendar-button">
+            <div className="calendar-background">
+              <div className="calendar-icon" />
             </div>
-            <div className="profile-picture"></div>
+          </Link>
+
+          <SearchBar/>
+
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            {newNotifications ? (
+                <button className="notifications-button-new" onClick={() => setNewNotifications(false)}>
+                  <div className="notifications-new-background"/>
+                </button>
+            ) : (
+                <button className="notifications-button" onClick={() => setNewNotifications(true)}>
+                  <div className="notifications-background"/>
+                </button>
+            )}
           </div>
+
+          {loggedIn ? (
+              <div className="profile-component">
+                <div className="profile-picture-header"></div>
+                <div className="username">Test user</div>
+                <div className="arrow-down"/>
+              </div>
+          ) : (
+              <div className="profile-component">
+                <div className="login">
+                  <div className="log-in-wrapper">
+                    <div className="log-in-text">LOGIN</div>
+                  </div>
+                </div>
+                <div className="sign-up">
+                  <div className="sign-up-wrapper">
+                    <div className="sign-up-text">SIGN UP</div>
+                  </div>
+                </div>
+              </div>
+          )}
         </div>
       </div>
   );
