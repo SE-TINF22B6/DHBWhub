c64dde82ffb4e8e28b4289dbfab02d45fcddfebf
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-10-29 12:05:38 +0100

    App.tsx: Created routes for the pages

--- src/main/web/src/App.tsx
+++ src/main/web/src/App.tsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import { Home } from './scenes/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Home} from './scenes/Home';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Friends} from "./scenes/Friends";
+import {FAQ} from "./scenes/FAQ";
+import {Post} from "./scenes/Post";
+import {Contact} from "./scenes/Contact";
+import {Calendar} from "./scenes/Calendar";
+import {PrivacyPolicy} from "./scenes/PrivacyPolicy";
 
 class App extends Component {
   public render(): JSX.Element {
     return (
         <Router>
           <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<Home/>}/>
+            <Route path='/friends' element={<Friends/>}/>
+            <Route path='/faq' element={<FAQ/>}/>
+            <Route path='/post' element={<Post/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/calendar' element={<Calendar/>}/>
+            <Route path='/privacy-policy' element={<PrivacyPolicy/>}/>
           </Routes>
         </Router>
     );
