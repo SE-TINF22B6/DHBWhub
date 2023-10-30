e2a7f6a442fd4f7ac34405d2a6d7c94cbbdab5fc
denniskp <122602355+denniskp@users.noreply.github.com>
2023-10-30 13:00:58 +0100

    fix: add response status

--- src/main/java/de/tinf22b6/dhbwhub/exception/NoSuchEntryException.java
+++ src/main/java/de/tinf22b6/dhbwhub/exception/NoSuchEntryException.java
@@ -1,5 +1,9 @@
 package de.tinf22b6.dhbwhub.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchEntryException extends RuntimeException {
     public NoSuchEntryException(String message) {
         super(message);
