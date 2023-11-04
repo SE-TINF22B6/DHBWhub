f2ddfe19e8e2c8f5d209a8d544b4f084c4ae60fe
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-11-04 20:01:42 +0100

    Added default components contact, faq and privacy policy

--- src/main/web/src/scenes/Contact/index.tsx
+++ src/main/web/src/scenes/Contact/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./index.css";
 import {Header} from "../Home/components/Header";
 
-export const PrivacyPolicy = (): JSX.Element => {
+export const Contact = (): JSX.Element => {
   return (
-      <div className="privacy-policy">
+      <div className="contact">
         <Header></Header>
       </div>
   );
