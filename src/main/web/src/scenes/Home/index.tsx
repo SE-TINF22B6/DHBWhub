a0a0c55cf2db9b155e08febf7bea61c321dd9fd5
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2023-11-05 01:30:56 +0100

    Edited index.tsx in Home

--- src/main/web/src/scenes/Home/index.tsx
+++ src/main/web/src/scenes/Home/index.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import "./index.css";
 import {Header} from "./components/Header";
-import {Events} from "./components/Events";
-import {CreatePost} from "./components/CreatePost";
-import {Posts} from "./components/Posts";
-import {Infos} from "./components/Infos";
-import {SavedPosts} from "./components/SavedPosts";
-import {SortOptions} from "./components/SortOptions";
-import {PopularTags} from "./components/PopularTags";
-import ModalComponent from "./components/Modal";
 
 export const Home = (): JSX.Element => {
   return (
