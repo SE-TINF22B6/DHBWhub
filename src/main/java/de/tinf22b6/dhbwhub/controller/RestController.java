71f8097f4742735f524ce9f36627de1aa4716f56
denniskp <122602355+denniskp@users.noreply.github.com>
2023-11-07 12:42:21 +0100

    refactor: move to correct package

--- src/main/java/de/tinf22b6/dhbwhub/controller/RestController.java
+++ src/main/java/de/tinf22b6/dhbwhub/controller/RestController.java
@@ -1,16 +1,14 @@
-package de.tinf22b6.dhbwhub.controllers;
+package de.tinf22b6.dhbwhub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-//@Controller
-//@CrossOrigin(origins = "http://localhost:3000")
-//@RequestMapping("login")
+@Controller
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("login")
 public class RestController {
-
     @PostMapping("/email")
     public void receiveEmailAddress(@RequestBody String email) {
         System.out.println(email);
     }
-
 }
