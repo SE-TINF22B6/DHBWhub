d222e0c5cbc512ccc767a86347dbeddec9721aa0
FurKay00 <furkay68@outlook.com>
2023-11-08 16:36:46 +0100

    maybe fixed test issues

--- src/test/java/de/tinf22b6/dhbwhub/DHBWhubApplicationTests.java
+++ src/test/java/de/tinf22b6/dhbwhub/DHBWhubApplicationTests.java
@@ -3,11 +3,14 @@ package de.tinf22b6.dhbwhub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class DHBWhubApplicationTests {
+class DHBWhubApplicationTest {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
