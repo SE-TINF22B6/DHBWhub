69867b26b5dfccc0c92d1fd36b8c2f5746a9275d
denniskp <122602355+denniskp@users.noreply.github.com>
2023-11-09 09:59:41 +0100

    feat: add tests

--- src/test/java/de/tinf22b6/dhbwhub/repository/FacultyRepositoryTests.java
+++ src/test/java/de/tinf22b6/dhbwhub/repository/FacultyRepositoryTests.java
@@ -1,35 +1,76 @@
 package de.tinf22b6.dhbwhub.repository;
 
 import de.tinf22b6.dhbwhub.model.Faculty;
-import de.tinf22b6.dhbwhub.repository.interfaces.SpringFacultyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @ActiveProfiles("test")
+@ComponentScan(basePackages = "de.tinf22b6.dhbwhub.repository")
 public class FacultyRepositoryTests {
     @Autowired
-    private SpringFacultyRepository facultyRepository;
+    private FacultyRepository facultyRepository;
 
     @Test
-    void TestFindAll() {
+    void FindAll_HasSize_Two() {
         // Arrange
-        Faculty faculty = new Faculty("TINF22B6");
+        Faculty faculty1 = new Faculty("Informatik");
+        Faculty faculty2 = new Faculty("Jura");
 
         // Act
-        facultyRepository.save(faculty);
+        facultyRepository.save(faculty1);
+        facultyRepository.save(faculty2);
 
         // Assert
-        assertThat(facultyRepository.findAll()).isNotEmpty();
+        assertThat(facultyRepository.findAll()).hasSize(2);
+    }
+
+    @Test
+    void FindAll_IsEmpty_True() {
+        assertThat(facultyRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    void Find_IsNotNull_True() {
+        Faculty faculty = new Faculty("Informatik");
+
+        facultyRepository.save(faculty);
+
+        assertThat(facultyRepository.find(faculty.getFacId())).isNotNull();
+    }
+
+    @Test
+    void Find_IsNull_True() {
+        assertThat(facultyRepository.find(1L)).isNull();
+    }
+
+    @Test
+    void Save_HasSize_One() {
+        Faculty faculty = new Faculty("Informatik");
+
+        facultyRepository.save(faculty);
+
+        assertThat(facultyRepository.findAll()).hasSize(1);
+    }
+
+    @Test
+    void Delete_SizeChange() {
+        Faculty faculty = new Faculty("Informatik");
+
+        facultyRepository.save(faculty);
+
+        assertThat(facultyRepository.findAll()).hasSize(1);
+
+        facultyRepository.delete(faculty.getFacId());
+
+        assertThat(facultyRepository.findAll()).isEmpty();
     }
 }
