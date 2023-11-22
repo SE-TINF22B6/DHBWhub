158f8d86fb2317b3dd246f2e1385bac04bb408c8
jonas-ebbinghaus <jonas.ebbinghaus@vaillant-group.com>
2023-11-22 19:20:53 +0100

    Update branch

--- src/main/web/src/scenes/Home/components/header/Header.tsx
+++ src/main/web/src/scenes/Home/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { SearchBar } from './SearchBar';
+import {SearchBar} from './SearchBar';
 
 import {Link, useLocation} from 'react-router-dom';
 import "./Header.css";
@@ -9,65 +9,49 @@ import SignUp from "../signup/SignUp";
 
 export const Header = () => {
 
-  const [newNotifications, setNewNotifications] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState('');
-  const location = useLocation();
+    const [newNotifications, setNewNotifications] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [currentLocation, setCurrentLocation] = useState('');
+    const location = useLocation();
 
-  useEffect(() => {
-    setCurrentLocation(location.pathname);
-  }, [location]);
+    useEffect(() => {
+        setCurrentLocation(location.pathname);
+    }, [location]);
 
-  return (
-      <div className="header">
-        <div className="header-content">
-          <Link to="/" className="home-button">
-            <Logo/>
-          </Link>
-          <Link to="/" className="home-button">
-            <div className={`home-background ${currentLocation === '/' ? 'active' : ''}`}></div>
-          </Link>
-          <Link to="/friends" className="friends-button">
-            <div className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`}></div>
-          </Link>
-          <Link to="/calendar" className="calendar-button">
-            <div className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`}/>
-          </Link>
+    return (
+        <div className="header">
+            <div className="header-content">
+                <Link to="/" className="home-button">
+                    <Logo/>
+                </Link>
+                <Link to="/" className="home-button">
+                    <div className={`home-background ${currentLocation === '/' ? 'active' : ''}`}></div>
+                </Link>
+                <Link to="/friends" className="friends-button">
+                    <div className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`}></div>
+                </Link>
+                <Link to="/calendar" className="calendar-button">
+                    <div className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`}/>
+                </Link>
 
-          <SearchBar/>
+                <SearchBar/>
 
-          <div style={{display: 'flex', alignItems: 'center'}}>
-            {newNotifications ? (
-                <button className="notifications-button-new" onClick={() => setNewNotifications(false)}>
-                  <div className="notifications-new-background"/>
-                </button>
-            ) : (
-                <button className="notifications-button" onClick={() => setNewNotifications(true)}>
-                  <div className="notifications-background"/>
-                </button>
-            )}
-          </div>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    {newNotifications ? (
+                        <button className="notifications-button-new" onClick={() => setNewNotifications(false)}>
+                            <div className="notifications-new-background"/>
+                        </button>
+                    ) : (
+                        <button className="notifications-button" onClick={() => setNewNotifications(true)}>
+                            <div className="notifications-background"/>
+                        </button>
+                    )}
+                </div>
 
-          {loggedIn ? (
-              <div className="profile-component">
-                <div className="profile-picture-header"></div>
-                <div className="username">Test user</div>
-                <button className="header-menu" onClick={() => setLoggedIn(false)}>
-                  <div className="arrow-down"/>
-                </button>
-              </div>
-          ) : (
-              <div className="profile-component">
+
                 <Login/>
                 <SignUp/>
-                <button className="sign-up" onClick={() => setLoggedIn(true)}>
-                  <div className="sign-up-wrapper">
-                    <div className="sign-up-text">SIGN UP</div>
-                  </div>
-                </button>
-              </div>
-          )}
+            </div>
         </div>
-      </div>
-  );
+    );
 };
