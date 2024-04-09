24c2cba155c9c4284f49ad539f09991b36c6d8e9
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-04-09 22:02:44 +0200

    Adapt auth

--- src/main/java/de/tinf22b6/dhbwhub/controller/AuthController.java
+++ src/main/java/de/tinf22b6/dhbwhub/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -46,7 +47,7 @@ public class AuthController {
 
         UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
         List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+                .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(new JwtResponse(jwt,
