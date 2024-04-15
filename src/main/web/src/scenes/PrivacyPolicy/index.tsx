1dc24e9105df02630d7e4caaa575306e9b9407bb
Max Schwinghammer <maxschwinghammer@outlook.com>
2024-04-15 22:28:47 +0200

    tried to fix error

--- src/main/web/src/scenes/PrivacyPolicy/index.tsx
+++ src/main/web/src/scenes/PrivacyPolicy/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./index.css";
-import {Header} from "../Home/components/header/Header";
 import {Header} from "../../organisms/header/Header";
 
 export const PrivacyPolicy = () => {
   return (
       <div className="privacy-policy">
         <Header></Header>
-      <div className="privacy-policy-component">
-        <Header/>
-        <Footer/>
+        <div className="privacy-policy-component">
+          <Header/>
+        </div>
       </div>
   );
-};
+}
