8a254a415231d4a690e0c5ff069c42ba607720fc
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-04-15 17:32:20 +0200

    Improve header and general design

--- src/main/web/src/scenes/PageNotFound/index.tsx
+++ src/main/web/src/scenes/PageNotFound/index.tsx
@@ -3,14 +3,16 @@ import { Header } from "../../organisms/header/Header";
 import "./index.css";
 import Lottie from "lottie-react";
 import errorAnimationData from "../../assets/error.json";
+import {Footer} from "../../organisms/footer/Footer";
 
 export const PageNotFound = () => {
-    return (
-        <div className="page-not-found">
-          <Header/>
-          <div className="error-animation">
-            <Lottie animationData={errorAnimationData}/>
-          </div>
+  return (
+      <div className="page-not-found">
+        <Header/>
+        <div className="error-animation">
+          <Lottie animationData={errorAnimationData}/>
         </div>
-    );
+        <Footer/>
+      </div>
+  );
 }
