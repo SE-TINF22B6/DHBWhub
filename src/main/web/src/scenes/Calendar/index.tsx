ecaf7c63e32348cf2b95439c43b2f3cc891ec983
Max Schwinghammer <maxschwinghammer@outlook.com>
2024-04-15 22:33:19 +0200

    Update index.tsx

--- src/main/web/src/scenes/Calendar/index.tsx
+++ src/main/web/src/scenes/Calendar/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./index.css";
-import {Header} from "../Home/components/header/Header";
 import {Header} from "../../organisms/header/Header";
 
 export const Calendar = (): JSX.Element => {
