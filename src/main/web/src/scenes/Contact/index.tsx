6a39f24a7fd886e50b36f2df308ca830b1f78981
Max Schwinghammer <maxschwinghammer@outlook.com>
2024-04-15 22:33:31 +0200

    Update index.tsx

--- src/main/web/src/scenes/Contact/index.tsx
+++ src/main/web/src/scenes/Contact/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import {Header} from "../../organisms/header/Header";
-import {Footer} from "../../organisms/footer/Footer";
 
 export const Contact = () => {
   return (
