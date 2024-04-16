480ed92ad6881a49a264764150a27f7fc2a77d17
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-04-16 16:37:48 +0200

    Fixed React CI

--- src/main/web/src/scenes/Search/index.tsx
+++ src/main/web/src/scenes/Search/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import {Header} from "../Home/components/header/Header";
+import {Header} from "../../organisms/header/Header";
 
 export const Search = (): JSX.Element => {
   return (
