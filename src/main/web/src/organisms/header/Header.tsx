33b029939d0265bfcad74227dd3147aa6e2353da
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-04-16 17:34:06 +0200

    Edited header and added notifications

--- src/main/web/src/organisms/header/Header.tsx
+++ src/main/web/src/organisms/header/Header.tsx
@@ -3,10 +3,12 @@ import {SearchBar} from './SearchBar';
 import {Link, useLocation} from 'react-router-dom';
 import Login from '../../scenes/Home/components/login/Login';
 import SignUp from "../../scenes/Home/components/signup/SignUp";
+import {Notifications} from "./Notifications";
 import "./Header.css";
 
 export const Header = () => {
 
+  const [showNotifications, setShowNotifications] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [currentLocation, setCurrentLocation] = useState('');
   const location = useLocation();
@@ -15,6 +17,9 @@ export const Header = () => {
     setCurrentLocation(location.pathname);
   }, [location]);
 
+  const handleNotificationsButtonClick = (): void => {
+    setShowNotifications(!showNotifications);
+  };
   return (
       <div className="header">
         <Link to="/" aria-label="To the homepage">
@@ -23,21 +28,29 @@ export const Header = () => {
             <span>hub</span>
           </div>
         </Link>
-        <Link to="/" aria-label="To the homepage">
-          <div className={`home-background ${currentLocation === '/' ? 'active' : ''}`}></div>
+        <Link className={`home-background ${currentLocation === '/' ? 'active' : ''}`} to="/" aria-label="To the homepage">
+          <img alt="Home" src={process.env.PUBLIC_URL + '/assets/home/header/home.svg'}/>
         </Link>
-        <Link to="/friends" aria-label="To the friends page">
-          <div className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`}></div>
+        <Link className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`} to="/friends" aria-label="To the friends page">
+          <img alt="Friends" src={process.env.PUBLIC_URL + '/assets/home/header/friends.svg'}/>
         </Link>
-        <Link to="/calendar" aria-label="To the calendar page">
-          <div className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`}/>
+        <Link className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`} to="/calendar" aria-label="To the calendar page">
+          <img alt="Calendar" src={process.env.PUBLIC_URL + '/assets/home/header/calendar.svg'}/>
         </Link>
         <SearchBar/>
         <div className="notifications-button-container">
-          <button className="notifications-button" aria-label="Notifications-Button">
-            <div className="notifications-background" />
-          </button>
+        {showNotifications  ? (
+            <button className="notifications-button-new" onClick={handleNotificationsButtonClick}>
+              <img alt="New notifications" src={process.env.PUBLIC_URL + '/assets/home/header/notifications.svg'}/>
+              <img className="notifications-dot" alt="New notifications" src={process.env.PUBLIC_URL + '/assets/home/header/notifications-dot.svg'}/>
+            </button>
+        ) : (
+            <button className="notifications-button" onClick={handleNotificationsButtonClick} aria-label="Notifications-Button">
+              <img alt="New notifications" src={process.env.PUBLIC_URL + '/assets/home/header/notifications.svg'}/>
+            </button>
+        )}
         </div>
+        {showNotifications && <Notifications showNotifications={showNotifications} />}
         {loggedIn ? (
             <div className="profile-component">
               <Link to="/profile" aria-label="To the profile" onClick={() => setLoggedIn(false)}>
