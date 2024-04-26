a0a9ea35e07a89a77782d70b696a4794af28521a
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-04-26 14:49:34 +0200

    Added Tag as atom

--- src/main/web/src/atoms/Tag.tsx
+++ src/main/web/src/atoms/Tag.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Tag.css';
 import {Link} from "react-router-dom";
 
-
 interface TagProps {
-  name: string
+  name: string;
   index: number;
+  isEventTag: boolean;
 }
 
 export const Tag : React.FC<TagProps> = (props: TagProps) => {
-  let {name, index} = props;
+  let {name, index, isEventTag} = props;
 
   return (
       <Link key={index} to={`/tag/?name=${name.toLowerCase().replace(' ', '-')}`} className="tag-button">
-        <div className="tag">{name}</div>
+        <div className={isEventTag ? "event-tag" : "tag"}>{name}</div>
       </Link>
   );
 };
