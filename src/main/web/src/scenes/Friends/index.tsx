0992577e28e2d9f630d80a972dd91416b196c2da
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-02 22:18:06 +0200

    Implemented scroll up button

--- src/main/web/src/scenes/Friends/index.tsx
+++ src/main/web/src/scenes/Friends/index.tsx
@@ -2,15 +2,10 @@ import React from "react";
 import "./index.css";
 import {Header} from "../../organisms/header/Header";
 import {Footer} from "../../organisms/footer/Footer";
-import AdBlockOverlay from "../../organisms/ad-block-overlay/AdBlockOverlay";
-import {useDetectAdBlock} from "adblock-detect-react";
-import {usePreventScrolling} from "../../organisms/ad-block-overlay/preventScrolling";
 
 export const Friends = () => {
-
   return (
       <div className="friends">
-        {adBlockDetected && <AdBlockOverlay/>}
         <Header/>
         <h1 className="loading">Work in progress</h1>
         <Footer/>
