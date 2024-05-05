5ade0f1a5ca3ae63ebb8318a4241cc25a62264c9
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-05 18:53:01 +0200

    Added dummyPosts

---
+++ src/main/web/src/scenes/Home/components/saved-posts/dummyPosts.tsx
@@ -0,0 +1,8 @@
+export const dummyPosts: string[] = [
+  'Gymfinder: So findet ihr die besten Gyms!',
+  'Diese Future Skills sind essenziell!',
+  '1 Jahr und 4 Monate - Röthigs neuer Rekord!',
+  'So viel Koffein solltest du täglich nehmen!',
+  '15 Vorteile, nach dem Aufstehen ein lecker Bierchen zu trinken',
+  'Wie man erfolgreich Leads generiert!'
+];
