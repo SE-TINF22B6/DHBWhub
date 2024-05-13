5135df962942315ca86b3c1558bafd831f5c902f
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-05-14 00:00:44 +0200

    adapt signup dialog + add email verification functionality

--- src/test/web/service/auth.service.text.mjs
+++ src/test/web/service/auth.service.text.mjs
@@ -1,12 +1,11 @@
 import axios from "axios";
-import * as AuthService from "./auth.service"; // Importieren Sie den zu testenden Service
+import * as AuthService from "./auth.service";
 
-// Mocken Sie axios, um Netzwerkanfragen zu simulieren
 jest.mock("axios");
 
 describe("AuthService Tests", () => {
     test("register function should make a POST request to signup endpoint", async () => {
-        // Mocken Sie die Antwort von axios.post für die Registrierung
+
         axios.post.mockResolvedValue({ data: { message: "User registered successfully" } });
 
         const response = await AuthService.register("testUser", "password");
@@ -19,7 +18,7 @@ describe("AuthService Tests", () => {
     });
 
     test("login function should make a POST request to login endpoint", async () => {
-        // Mocken Sie die Antwort von axios.post für das Login
+
         axios.post.mockResolvedValue({
             data: { accessToken: "testAccessToken", username: "testUser" },
         });
@@ -33,5 +32,5 @@ describe("AuthService Tests", () => {
         );
     });
 
-    // Weitere Tests für andere Funktionen folgen dem gleichen Muster
+    test();
 });
