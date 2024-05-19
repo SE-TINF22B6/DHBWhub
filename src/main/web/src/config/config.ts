5f3cb451ee1af3db1352a0e4361a1e4b7b1121d5
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-05-19 22:12:41 +0200

    adapt signup dialog + add email verification functionality

--- src/main/web/src/config/config.ts
+++ src/main/web/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // apiUrl: 'http://localhost:8080/', // For test purposes only
-  apiUrl: 'http://193.196.38.232:8080/',
+  apiUrl: 'http://localhost:8080/', // For test purposes only
+  // apiUrl: 'http://193.196.38.232:8080/',
 };
 
 export default config;
--- src/main/web/src/organisms/signup/EmailInput.tsx
+++ src/main/web/src/organisms/signup/EmailInput.tsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import './EmailInput.css';
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import {Formik, Field, Form, ErrorMessage} from "formik";
 import * as Yup from "yup";
 import {emailVerification} from "../../services/AuthService";
+import ErrorModal from "./ErrorModal";
 
 const EmailInput = ({onSuccess}: any) => {
     const [message, setMessage] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
+    const [showError, setShowError] = useState<boolean>(false);
+    const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
 
     const initialValues: {
         email: string;
@@ -17,6 +20,10 @@ const EmailInput = ({onSuccess}: any) => {
     const handleOpenLogin = () => {
 
     }
+
+    const handleInputFocus = () => {
+        setShowError(false);
+    };
 
     const validationSchema = Yup.object().shape({
         email: Yup.string()
@@ -45,8 +52,10 @@ const EmailInput = ({onSuccess}: any) => {
                         error.message ||
                         error.toString();
 
-                    if (error.message === "Error: Email is already in use!") {
-                        resMessage = "The Email is already in use, please try again.";
+                    console.log("Errormessgae: " + error.message)
+
+                    if (error.message === "Request failed with status code 400") {
+                        setShowError(true);
                     }
 
                     setLoading(false);
@@ -63,33 +72,46 @@ const EmailInput = ({onSuccess}: any) => {
                 onSubmit={handleVerify}
             >
                 <Form>
-                        <div className="modalHeader">
-                            <h3 className="modalHeadline">CREATE YOUR ACCOUNT</h3>
-                        </div>
-                        <p className="info-text">Please note that it is required to verify your email address by entering a verification
-                            code.</p>
-                        <div className="form-group">
-                            <label htmlFor="email" className="heading">Email</label>
-                            <Field name="email" type="text" className="form-control"/>
-                            <ErrorMessage
-                                name="email"
-                                component="div"
-                                className="alert-danger"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="loading-btn">
-                                {loading && (
-                                    <span className="spinner-border spinner-border-sm"></span>
-                                )}
-                                <span className="btn-text">CONTINUE</span>
-                            </button>
-                        </div>
+                    <div className="modalHeader">
+                        <h3 className="modalHeadline">CREATE YOUR ACCOUNT</h3>
+                    </div>
+                    <p className="info-text">Please note that it is required to verify your email address by entering a
+                        verification
+                        code.</p>
+                    <div className="form-group">
+                        <label htmlFor="email" className="heading">Email</label>
+                        <Field
+                            name="email"
+                            type="text"
+                            className="form-control"
+                            onFocus={() => setIsInputFocused(true)}
+                            onBlur={() => setIsInputFocused(false)}
+                        />
+                        <ErrorMessage
+                            name="email"
+                            component="div"
+                            className="alert-danger"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="loading-btn">
+                            {loading && (
+                                <span className="spinner-border spinner-border-sm"></span>
+                            )}
+                            <span className="btn-text">CONTINUE</span>
+                        </button>
+                    </div>
 
-                        <div className="signup-option">
-                            <label className="signup-option-text">Already have an account? </label>
-                            <label className="signup-option-text-link" onClick={handleOpenLogin}>LOGIN</label>
-                        </div>
+                    <div className="signup-option">
+                        <label className="signup-option-text">Already have an account? </label>
+                        <label className="signup-option-text-link" onClick={handleOpenLogin}>LOGIN</label>
+                    </div>
+
+                    <div className="error-message-dialog">
+                        {showError && !isInputFocused && (
+                            <ErrorModal message={message} onClose={() => setShowError(false)}/>
+                        )}
+                    </div>
                 </Form>
             </Formik>
         </div>
