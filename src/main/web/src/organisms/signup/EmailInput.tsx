09f08941bdf62b6cdc2a823c811d8fb2c8b51728
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-05-19 22:12:41 +0200

    adapt signup dialog + add email verification functionality

--- src/main/web/src/organisms/signup/EmailInput.tsx
+++ src/main/web/src/organisms/signup/EmailInput.tsx
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
 import './EmailInput.css';
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import {Formik, Field, Form, ErrorMessage} from "formik";
 import * as Yup from "yup";
 import {emailVerification} from "../../services/AuthService";
+import ErrorModal from "./ErrorModal";
 import {GoogleLogin} from "@react-oauth/google";
 
 const EmailInput = ({onSuccess}: any) => {
     const [message, setMessage] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
+    const [showError, setShowError] = useState<boolean>(false);
+    const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
 
     const initialValues: {
         email: string;
@@ -18,6 +21,10 @@ const EmailInput = ({onSuccess}: any) => {
     const handleOpenLogin = () => {
 
     }
+
+    const handleInputFocus = () => {
+        setShowError(false);
+    };
 
     const validationSchema = Yup.object().shape({
         email: Yup.string()
@@ -46,8 +53,8 @@ const EmailInput = ({onSuccess}: any) => {
                         error.message ||
                         error.toString();
 
-                    if (error.message === "Error: Email is already in use!") {
-                        resMessage = "The Email is already in use, please try again.";
+                    if (error.message === "Request failed with status code 400") {
+                        setShowError(true);
                     }
 
                     setLoading(false);
@@ -58,52 +65,66 @@ const EmailInput = ({onSuccess}: any) => {
 
     return (
         <div className="email-input-modal-content">
-          <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={handleVerify}
-          >
-            <Form>
-              <div className="modalHeader">
-                <h3 className="modalHeadline">CREATE YOUR ACCOUNT</h3>
-              </div>
-              <p className="info-text">Please note that it is required to verify your email address by entering a verification
-                code.</p>
-              <div className="form-group">
-                <label htmlFor="email" className="heading">Email</label>
-                <Field name="email" type="text" className="form-control"/>
-                <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="alert-danger"
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleVerify}
+            >
+                <Form>
+                    <div className="modalHeader">
+                        <h3 className="modalHeadline">CREATE YOUR ACCOUNT</h3>
+                    </div>
+                    <p className="info-text">Please note that it is required to verify your email address by entering a
+                        verification
+                        code.</p>
+                    <div className="form-group">
+                        <label htmlFor="email" className="heading">Email</label>
+                        <Field
+                            name="email"
+                            type="text"
+                            className="form-control"
+                            onFocus={() => setIsInputFocused(true)}
+                            onBlur={() => setIsInputFocused(false)}
+                        />
+                        <ErrorMessage
+                            name="email"
+                            component="div"
+                            className="alert-danger"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="loading-btn">
+                            {loading && (
+                                <span className="spinner-border spinner-border-sm"></span>
+                            )}
+                            <span className="btn-text">CONTINUE</span>
+                        </button>
+                    </div>
+
+                    <div className="signup-option">
+                        <label className="signup-option-text">Already have an account? </label>
+                        <label className="signup-option-text-link" onClick={handleOpenLogin}>LOGIN</label>
+                    </div>
+
+                    <div className="error-message-dialog">
+                        {showError && !isInputFocused && (
+                            <ErrorModal message={message} onClose={() => setShowError(false)}/>
+                        )}
+                    </div>
+                </Form>
+            </Formik>
+
+            <div className="google-oauth-signup">
+                <GoogleLogin size={'medium'} logo_alignment={'center'} ux_mode={'popup'} useOneTap={true}
+                             text={"signup_with"}
+                             onSuccess={credentialResponse => {
+                                 console.log(credentialResponse);
+                             }}
+                             onError={() => {
+                                 console.log('Login Failed');
+                             }}
                 />
-              </div>
-              <div className="form-group">
-                <button type="submit" className="loading-btn">
-                  {loading && (
-                      <span className="spinner-border spinner-border-sm"></span>
-                  )}
-                  <span className="btn-text">CONTINUE</span>
-                </button>
-              </div>
-
-              <div className="signup-option">
-                <label className="signup-option-text">Already have an account? </label>
-                <label className="signup-option-text-link" onClick={handleOpenLogin}>LOGIN</label>
-              </div>
-            </Form>
-          </Formik>
-
-          <div className="google-oauth-signup">
-            <GoogleLogin size={'medium'} logo_alignment={'center'} ux_mode={'popup'} useOneTap={true} text={"signup_with"}
-                         onSuccess={credentialResponse => {
-                           console.log(credentialResponse);
-                         }}
-                         onError={() => {
-                           console.log('Login Failed');
-                         }}
-            />
-          </div>
+            </div>
         </div>
     );
 };
