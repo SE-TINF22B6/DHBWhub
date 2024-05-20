52e14d4af5f183acbf3af40e75730decd11b9480
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-05-20 22:25:30 +0200

    add function to get profile picture

--- src/main/web/src/services/ProfilePictureService.tsx
+++ src/main/web/src/services/ProfilePictureService.tsx
@@ -2,5 +2,5 @@ import axios from "axios";
 import config from "../config/config";
 
 export const profilePicture = (username: string) => {
-    return axios.get(config.apiUrl + "/picture");
+    return axios.get(config.apiUrl + "/picture", { params: { username: username } });
 };
