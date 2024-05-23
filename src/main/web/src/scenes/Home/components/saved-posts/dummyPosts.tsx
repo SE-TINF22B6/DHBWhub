146332014b7061760fffa61bacdca3842699d296
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-23 12:57:49 +0200

    Added dummyPosts for saved posts

--- src/main/web/src/scenes/Home/components/saved-posts/dummyPosts.tsx
+++ src/main/web/src/scenes/Home/components/saved-posts/dummyPosts.tsx
@@ -1,8 +1,34 @@
-export const dummyPosts: string[] = [
-  'Gymfinder: So findet ihr die besten Gyms!',
-  'Diese Future Skills sind essenziell!',
-  '1 Jahr und 4 Monate - Röthigs neuer Rekord!',
-  'So viel Koffein solltest du täglich nehmen!',
-  '15 Vorteile, nach dem Aufstehen ein lecker Bierchen zu trinken',
-  'Wie man erfolgreich Leads generiert!'
+import {SavedPostModel} from "./model/SavedPostModel";
+
+export const dummyPosts: SavedPostModel[] = [
+  {
+    postId: 1,
+    title: 'Gymfinder: How to find the best gyms!',
+    savedPostId: 1,
+  },
+  {
+    postId: 2,
+    title: 'These future skills are essential!',
+    savedPostId: 2,
+  },
+  {
+    postId: 3,
+    title: '1 year and 4 months - Röthigs new record!',
+    savedPostId: 3,
+  },
+  {
+    postId: 4,
+    title: 'You should have this much caffeine every day!',
+    savedPostId: 4,
+  },
+  {
+    postId: 5,
+    title: '15 benefits of drinking a delicious beer after getting up',
+    savedPostId: 5,
+  },
+  {
+    postId: 6,
+    title: 'How to successfully generate leads!',
+    savedPostId: 6,
+  },
 ];
