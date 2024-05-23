eeea75a0f02ca2750731f3881ccd5f59984caf29
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-23 12:54:21 +0200

    Added @CrossOrigin

--- src/main/java/de/tinf22b6/dhbwhub/controller/PictureController.java
+++ src/main/java/de/tinf22b6/dhbwhub/controller/PictureController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/picture")
 public class PictureController {
     private final PictureService service;
