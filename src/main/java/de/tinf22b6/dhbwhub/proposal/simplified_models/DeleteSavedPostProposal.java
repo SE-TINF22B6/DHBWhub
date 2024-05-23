c3d8616688192959938bc6634eb5b8ed6206b9c7
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-23 12:48:40 +0200

    Added missing repo

---
+++ src/main/java/de/tinf22b6/dhbwhub/proposal/simplified_models/DeleteSavedPostProposal.java
@@ -0,0 +1,15 @@
+package de.tinf22b6.dhbwhub.proposal.simplified_models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteSavedPostProposal {
+    private Long userId;
+    private Long postId;
+}
