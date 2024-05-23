509dfe6db73145f177be40c6f224248c02ec4918
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-23 12:43:52 +0200

    Added profile picture service, saved posts adaptions

--- src/main/java/de/tinf22b6/dhbwhub/payload/response/JwtResponse.java
+++ src/main/java/de/tinf22b6/dhbwhub/payload/response/JwtResponse.java
@@ -1,19 +1,31 @@
 package de.tinf22b6.dhbwhub.payload.response;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 public class JwtResponse {
 
     private String token;
     private String type = "Bearer";
-    private Long id;
+    @Getter
+    @Setter
+    private Long accountId;
+    @Getter
+    @Setter
+    private Long userId;
+    @Setter
+    @Getter
     private String username;
+    @Setter
+    @Getter
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, Long accountId, Long userId, String username, String email, List<String> roles) {
         this.token = accessToken;
-        this.id = id;
+        this.accountId = accountId;
+        this.userId = userId;
         this.username = username;
         this.email = email;
         this.roles = roles;
@@ -33,30 +45,6 @@ public class JwtResponse {
 
     public void setTokenType(String tokenType) {
         this.type = tokenType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public List<String> getRoles() {
