48a9cd59dc38986d0d84ecb8cdbfb138ca19aae7
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-29 15:03:33 +0200

    Style refactorings, implemented ads and adblocker popup, added comment menu to report comment

--- src/main/web/src/organisms/comment/CommentModel.tsx
+++ src/main/web/src/organisms/comment/CommentModel.tsx
@@ -1,10 +1,9 @@
 export interface CommentModel {
-  postId: number,
-  commentId: number,
-  text: string,
-  authorUsername: string | null,
-  authorImage: string | null,
-  accountId: number,
-  timestamp: number,
+  commentId: number;
+  text: string;
+  authorUsername: string | null;
+  authorImage: string | null;
+  accountId: number;
+  timestamp: number;
   likeAmount: number;
 }
