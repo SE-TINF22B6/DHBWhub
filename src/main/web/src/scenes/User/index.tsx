a9d602a93d86fc6ff767f6eae28ea168f154cd64
Nahlam4 <josha.schmitt@egoproducts.com>
2024-05-31 16:29:28 +0200

    further Initialize User-Page and revise PostDetail Page

--- src/main/web/src/scenes/User/index.tsx
+++ src/main/web/src/scenes/User/index.tsx
@@ -1,96 +1,126 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { Header } from "../../organisms/header/Header";
+import React, {useState, useEffect} from "react";
+import {useLocation} from "react-router-dom";
+import {Header} from "../../organisms/header/Header";
 import "./index.css";
 import Lottie from "lottie-react";
 import animationData from "../../assets/loading.json";
-import errorAnimationData from "../../assets/error.json";
 import {Footer} from "../../organisms/footer/Footer";
 import AdBlockOverlay from "../../organisms/ad-block-overlay/AdBlockOverlay";
-import { useDetectAdBlock } from "adblock-detect-react";
+import {useDetectAdBlock} from "adblock-detect-react";
 import {usePreventScrolling} from "../../organisms/ad-block-overlay/preventScrolling";
 import {MobileFooter} from "../../organisms/header/MobileFooter";
 import {useMediaQuery} from "@mui/system";
 import config from "../../config/config";
 import {getJWT} from "../../services/AuthService";
+import {UserDetail} from "./User";
+
 
 export const User = () => {
-  const location = useLocation();
-  const searchParams: URLSearchParams = new URLSearchParams(location.search);
-  const username: string | null = searchParams.get('name');
-  const [userData, setUserData] = useState();
+    const location = useLocation();
+    const searchParams: URLSearchParams = new URLSearchParams(location.search);
+    const id: string | null = searchParams.get('id');
+    const username: string | null = searchParams.get('name');
+    const course: string | null = searchParams.get('course');
+    const followers: string | null = searchParams.get('followers');
+    const [userData, setUserData] = useState();
 
-  const [notFound, setNotFound] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const isSmartphoneSize: boolean = useMediaQuery('(max-width: 412px)');
+    const [notFound, setNotFound] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const isSmartphoneSize: boolean = useMediaQuery('(max-width: 412px)');
 
-  const adBlockDetected: boolean = useDetectAdBlock();
-  usePreventScrolling(adBlockDetected);
+    const adBlockDetected: boolean = useDetectAdBlock();
+    usePreventScrolling(adBlockDetected);
 
-  const jwt: string | null = getJWT();
-  const headersWithJwt = {
-    ...config.headers,
-    'Authorization': jwt ? `Bearer ${jwt}` : ''
-  };
-
-  useEffect((): void => {
-    const fetchUser = async (): Promise<void> => {
-      try {
-        const response: Response = await fetch(config.apiUrl + `user/${username}`,{
-          headers: headersWithJwt
-        });
-        if (response.ok) {
-          const userData = await response.json();
-          setUserData(userData);
-        } else {
-          setNotFound(true);
-        }
-        setLoading(false);
-      } catch (error) {
-        console.error("Fehler beim Abrufen der Nutzerdaten:", error);
-        setNotFound(true);
-        setLoading(false);
-      }
+    const jwt: string | null = getJWT();
+    const headersWithJwt = {
+        ...config.headers,
+        'Authorization': jwt ? `Bearer ${jwt}` : ''
     };
 
-    fetchUser();
-  });
+    useEffect((): void => {
+            const fetchUser = async (): Promise<void> => {
 
-  if (loading) {
+                    try {
+                        let response;
+
+                        /*if (searchParams.get('name')) {
+                            response = await fetch(`${config.apiUrl}user/${searchParams.get('name')}`, {
+                                headers: headersWithJwt
+                            });*/
+                        if (id) {
+                            response = await fetch(`${config.apiUrl}user/${id}`, {
+                                headers: headersWithJwt
+                            });
+                        }
+
+                        if (response && response.ok) {
+                            const userData = await response.json();
+                            setUserData(userData);
+                        } else {
+                            setNotFound(true);
+                        }
+                        setLoading(false);
+                    } catch (error) {
+                        console.error("Fehler beim Abrufen der Nutzerdaten:", error);
+                        setNotFound(true);
+                        setLoading(false);
+                    }
+                }
+            ;
+
+            fetchUser();
+        }
+    )
+    ;
+
+    if (loading) {
+        return (
+            <div className="page">
+                {adBlockDetected && <AdBlockOverlay/>}
+                <Header/>
+                <div className="loading-animation">
+                    <Lottie animationData={animationData}/>
+                </div>
+                <Footer/>
+                {isSmartphoneSize && <MobileFooter/>}
+            </div>
+        );
+    }
+
+    if (notFound || !username) {
+
+    }
+    const handleFollow = () => {
+        // Logik für das Folgen hier einfügen
+        console.log(`Du folgst ${username} jetzt!`);
+    };
+
     return (
         <div className="page">
-          {adBlockDetected && <AdBlockOverlay/>}
-          <Header/>
-          <div className="loading-animation">
-            <Lottie animationData={animationData}/>
-          </div>
-          <Footer/>
-          {isSmartphoneSize && <MobileFooter/>}
-        </div>
-    );
-  }
+            {adBlockDetected && <AdBlockOverlay/>}
+            <Header/>
+            <div className="user-component">
+                <div className="user-name">
+                    {userData} <UserDetail accountId={4} image={null} course={null} followers={null} username={"strange"}></UserDetail>
+                    {username}
+                </div>
+                <div className="user-name">
+                    {id}
+                </div>
+                <div>
+                    <p className="user-course">
+                        Kurs: </p> {course}
+                </div>
+                <div>
+                    <p className="user-followers">
+                        followers: </p> {followers}
 
-  if (notFound || !username) {
-    return (
-        <div className="page">
-          {adBlockDetected && <AdBlockOverlay/>}
-          <Header/>
-          <div className="error-animation">
-            <Lottie animationData={errorAnimationData}/>
-          </div>
-          <Footer/>
-          {isSmartphoneSize && <MobileFooter/>}
+                </div>
+                <button className="user-follow" onClick={handleFollow}>Follow</button>
+            </div>
+            <Footer/>
+            {isSmartphoneSize && <MobileFooter/>}
         </div>
-    );
-  }
 
-  return (
-      <div className="page">
-        {adBlockDetected && <AdBlockOverlay/>}
-        <Header/>
-        {userData}
-        <Footer/>
-        {isSmartphoneSize && <MobileFooter/>}
-      </div>
-  );
+    );
 };
