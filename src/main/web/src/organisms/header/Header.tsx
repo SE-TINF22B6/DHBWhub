63903f25d4261cb5748e9d50862fcbecd7ef24aa
FurKay00 <furkay68@outlook.com>
2024-06-07 13:25:35 +0200

    Feat: First attempt for notifications

--- src/main/web/src/organisms/header/Header.tsx
+++ src/main/web/src/organisms/header/Header.tsx
@@ -1,99 +1,91 @@
-import React, {useEffect, useState} from 'react';
-import {SearchBar} from './SearchBar';
-import {Link, useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { SearchBar } from './SearchBar';
+import { Link, useLocation } from 'react-router-dom';
 import ModalLoginContainer from '../login/ModalLoginContainer';
-import {Notifications} from "./Notifications";
+import { Notifications } from "./Notifications";
 import SignUp from "../signup/SignUp";
 import ProfilePictureService from "../../services/ProfilePictureService";
-import {isUserLoggedIn} from "../../services/AuthService";
+import { isUserLoggedIn } from "../../services/AuthService";
 import "./Header.css";
+import "./Notifications.css";
 import {Tooltip} from "react-tooltip";
 import config from "../../config/config";
 
 export const Header = () => {
-
     const [showNotifications, setShowNotifications] = useState(false);
     const [currentLocation, setCurrentLocation] = useState('');
     const [userImage, setUserImage] = useState(localStorage.getItem('userImage') || "data:image/svg+xml;base64,PHg==");
     const location = useLocation();
 
     useEffect((): void => {
-      setCurrentLocation(location.pathname);
-    }, [location]);
-
-    useEffect((): void => {
-      const fetchUserImage = async () => {
-        const image: string | null = await ProfilePictureService.fetchUserImage();
-        if (image) {
-          setUserImage(image);
-        }
-      };
-      fetchUserImage();
-    }, []);
-
-    useEffect((): void => {
         setCurrentLocation(location.pathname);
     }, [location]);
+
+    useEffect((): void => {
+        const fetchUserImage = async () => {
+            const image: string | null = await ProfilePictureService.fetchUserImage();
+            if (image) {
+                setUserImage(image);
+            }
+        };
+        fetchUserImage();
+    }, []);
 
     const handleNotificationsButtonClick = (): void => {
         setShowNotifications(!showNotifications);
     };
+
     return (
         <div className="header">
-          <Link to="/" aria-label="To the homepage">
-            <div className='logo'>
-              <span>DHBW </span>
-              <span>hub</span>
-            </div>
-          </Link>
-          <Link className={`home-background ${currentLocation === '/' ? 'active' : ''}`} to="/"
-                aria-label="To the homepage">
-            <img alt="Home" src={process.env.PUBLIC_URL + '/assets/header/home.svg'} style={{height: "26px", width: "26px"}}/>
-          </Link>
-          <Link className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`} to={isUserLoggedIn() ? '/friends' : '#'}
-                aria-label="To the friends page" data-tooltip-id="like" data-tooltip-content={config.tooltipMessage}>
-            <img alt="Friends" src={process.env.PUBLIC_URL + '/assets/header/friends.svg'} style={{height: "26px", width: "26px"}}/>
-          </Link>
-          {!isUserLoggedIn() && (
-              <Tooltip variant={"light"} id="friends" place="bottom"/>
-          )}
-          <Link className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`} to="/calendar"
-                aria-label="To the calendar page">
-            <img alt="Calendar" src={process.env.PUBLIC_URL + '/assets/header/calendar.svg'} style={{height: "26px", width: "24px"}}/>
-          </Link>
-          <SearchBar/>
-          <div className="notifications-button-container" data-tooltip-id="notifications" data-tooltip-content={config.tooltipMessage}>
-            {showNotifications ? (
-                <button className="notifications-button-new" onClick={handleNotificationsButtonClick} disabled={!isUserLoggedIn()}>
-                  <img alt="New notifications"
-                       src={process.env.PUBLIC_URL + '/assets/header/notifications.svg'} style={{height: "25px", width: "26px"}}/>
-                  <img className="notifications-dot" alt="New notifications"
-                       src={process.env.PUBLIC_URL + '/assets/header/notifications-dot.svg'} style={{height: "7px", width: "8px"}}/>
-                </button>
-            ) : (
-                <button className="notifications-button" onClick={handleNotificationsButtonClick}
-                        aria-label="Notifications-Button" disabled={!isUserLoggedIn()}>
-                  <img alt="New notifications" src={process.env.PUBLIC_URL + '/assets/header/notifications.svg'}
-                       style={{height: "25px", width: "26px"}}/>
-                </button>
+            <Link to="/" aria-label="To the homepage">
+                <div className='logo'>
+                    <span>DHBW </span>
+                    <span>hub</span>
+                </div>
+            </Link>
+            <Link className={`home-background ${currentLocation === '/' ? 'active' : ''}`} to="/"
+                  aria-label="To the homepage">
+                <img alt="Home" src={process.env.PUBLIC_URL + '/assets/header/home.svg'} style={{height: "26px", width: "26px"}}/>
+            </Link>
+            <Link className={`friends-background ${currentLocation === '/friends' ? 'active' : ''}`} to={isUserLoggedIn() ? '/friends' : '#'}
+                  aria-label="To the friends page" data-tooltip-id="like" data-tooltip-content={config.tooltipMessage}>
+                <img alt="Friends" src={process.env.PUBLIC_URL + '/assets/header/friends.svg'} style={{height: "26px", width: "26px"}}/>
+            </Link>
+            {!isUserLoggedIn() && (
+                <Tooltip variant={"light"} id="friends" place="bottom"/>
             )}
-          </div>
-          {!isUserLoggedIn() && (
-              <Tooltip variant={"light"} id="notifications" place="bottom" style={{ zIndex: 999 }}/>
-          )}
-          {showNotifications && <Notifications showNotifications={showNotifications}/>}
-          {isUserLoggedIn() ? (
-              <div className="profile-component">
-                <Link to="/profile" aria-label="To the profile">
-                  <img className="profile-picture-header" alt="Profile" src={userImage}/>
-                </Link>
-              </div>
-          ) : (
-              <div className="profile-component">
-                <ModalLoginContainer/>
-                <SignUp/>
-              </div>
-          )}
+            <Link className={`calendar-background ${currentLocation === '/calendar' ? 'active' : ''}`} to="/calendar"
+                  aria-label="To the calendar page">
+                <img alt="Calendar" src={process.env.PUBLIC_URL + '/assets/header/calendar.svg'} style={{height: "26px", width: "24px"}}/>
+            </Link>
+            <SearchBar/>
+            <div className="notifications-button-container" data-tooltip-id="notifications" data-tooltip-content={config.tooltipMessage}>
+                <button className={`notifications-button ${showNotifications ? 'active' : ''}`} onClick={handleNotificationsButtonClick}
+                        aria-label="Notifications-Button" disabled={!isUserLoggedIn()}>
+                    <img alt="New notifications" src={process.env.PUBLIC_URL + '/assets/header/notifications.svg'}
+                         style={{height: "25px", width: "26px"}}/>
+                    {showNotifications && (
+                        <img className="notifications-dot" alt="New notifications"
+                             src={process.env.PUBLIC_URL + '/assets/header/notifications-dot.svg'} style={{height: "7px", width: "8px"}}/>
+                    )}
+                </button>
+            </div>
+            {!isUserLoggedIn() && (
+                <Tooltip variant={"light"} id="notifications" place="bottom" style={{ zIndex: 999 }}/>
+            )}
+            {showNotifications && <Notifications showNotifications={showNotifications}/>}
+            {isUserLoggedIn() ? (
+                <div className="profile-component">
+                    <Link to="/profile" aria-label="To the profile">
+                        <img className="profile-picture-header" alt="Profile" src={userImage}/>
+                    </Link>
+                </div>
+            ) : (
+                <div className="profile-component">
+                    <ModalLoginContainer/>
+                    <SignUp/>
+                </div>
+            )}
         </div>
     );
 };
--- src/main/web/src/organisms/header/Notifications.tsx
+++ src/main/web/src/organisms/header/Notifications.tsx
@@ -1,15 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import {NavigateFunction, useNavigate} from "react-router-dom";
 
 interface NotificationsProps {
   showNotifications: boolean;
 }
 
-interface NavigatorWithBadge extends Navigator {
-  setAppBadge?: (count: number) => Promise<void>;
+interface Notification {
+  notificationId: number;
+  groupId: number;
+  text: string;
+  link: string;
+  type: string;
 }
 
 export const Notifications: React.FC<NotificationsProps> = ({ showNotifications }) => {
   const [notificationShown, setNotificationShown] = useState(false);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  let navigate: NavigateFunction = useNavigate();
+
+  const handleClick = (link : string):void => {
+    navigate(link);
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      // Replace this with your actual API call
+      const fetchedNotifications: Notification[] = [
+        {notificationId: 1, groupId: 1, text: 'Your Comment on “Tag des Dualen Masters” has been liked by 21 users', link: "/post/?id=9", type: ''},
+        {notificationId: 2, groupId: 4, text: 'Your Post “Tag des Dualen Masters” has been liked by 21 users', link: "/post/?id=4", type: '' },
+        {notificationId: 3, groupId: 9, text: 'You have 21 new follower now', link: '#', type: '' },
+      ];
+      setNotifications(fetchedNotifications);
+    };
+
+    if (showNotifications) {
+      fetchNotifications();
+    }
+  }, [showNotifications]);
 
   useEffect(() => {
     const showNotification = async () => {
@@ -17,16 +45,11 @@ export const Notifications: React.FC<NotificationsProps> = ({ showNotifications 
         if ('Notification' in window && showNotifications && !notificationShown) {
           const permission = await Notification.requestPermission();
           if (permission === 'granted') {
-            new Notification('DHBWhub', {
+            /*new Notification('DHBWhub', {
               body: 'Neue Benachrichtigung von DHBWhub.',
               icon: '/logo192.png',
             });
-
-            const navigatorWithBadge = navigator as NavigatorWithBadge;
-            if (navigatorWithBadge.setAppBadge) {
-              await navigatorWithBadge.setAppBadge(10);
-            }
-
+            */
             setNotificationShown(true);
           }
         }
@@ -39,7 +62,15 @@ export const Notifications: React.FC<NotificationsProps> = ({ showNotifications 
   }, [showNotifications, notificationShown]);
 
   return (
-      <div className="notifications">
+      <div className= "notifications">
+        <span className= "notifications-title"> Notifications </span>
+        <ul>
+          {notifications.map((notification) => (
+              <li key={notification.notificationId}>
+                <a onClick={() => handleClick(notification.link)}>{notification.text}</a>
+              </li>
+          ))}
+        </ul>
       </div>
   );
 };
