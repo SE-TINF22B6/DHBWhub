83ee075af0c3710039cf490383d5e7093c8270f5
denniskp <122602355+denniskp@users.noreply.github.com>
2024-06-09 19:52:06 +0200

    fix: build

--- src/main/web/src/setupTests.ts
+++ src/main/web/src/setupTests.ts
@@ -3,10 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock;
