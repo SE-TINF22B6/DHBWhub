f16c15e6ba63d30431614bd46a33257d264cfdf2
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-06-11 11:38:35 +0200

    Quickfix: Post style

--- src/main/web/src/config/config.ts
+++ src/main/web/src/config/config.ts
@@ -3,7 +3,7 @@ const config = {
   apiUrl: 'https://56e66ce8-2ac2-4635-982a-f19f20896303.ka.bw-cloud-instance.org:8443/',
   googleClientId: '973066251162-r60h517iddja3k756d2f6n8sng5nn24q.apps.googleusercontent.com',
   tooltipMessage: "Please sign up or log in to use this feature",
-  adsOn: true,
+  adsOn: false,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
