6ccc3ac6c177302866bb0a82df53d0b5e80e91b8
Nahlam4 <josha.schmitt@egoproducts.com>
2024-06-12 20:48:06 +0200

    final implementation of Follow-Button and reworking of User-Page

--- src/main/web/src/scenes/User/index.tsx
+++ src/main/web/src/scenes/User/index.tsx
@@ -13,13 +13,12 @@ export const UserPage = () => {
     const searchParams: URLSearchParams = new URLSearchParams(window.location.search);
     const id: string | null = searchParams.get('id');
     const senderId: number | null = getUserId();
-
     const [userId, setUserId] = useState<number | null>(null);
     const [username, setUserName] = useState<string | null>(null);
     const [picture, setPicture] = useState<{
         id: number | null;
         name: string | null;
-        imageData: string | null
+        imageData: string | null;
     }>({id: null, name: null, imageData: null});
     const [userPosts, setUserPosts] = useState<any[]>([]);
     const [amountFollower, setAmountFollower] = useState<number | null>(null);
@@ -27,8 +26,9 @@ export const UserPage = () => {
     const [description, setDescription] = useState<string | null>(null);
     const [course, setCourse] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
-    const [notFound, setNotFound] = useState(true);
-    const [follow, setFollow] = useState(true);
+    const [followLoading, setFollowLoading] = useState(true);
+    const [notFound, setNotFound] = useState(false);
+    const [follow, setFollow] = useState(false);
     const [postLoading, setPostsLoading] = useState(true);
     const [postsNotFound, setPostsNotFound] = useState(false);
     const jwt: string | null = getJWT();
@@ -38,9 +38,9 @@ export const UserPage = () => {
     };
 
     useEffect(() => {
+        setFollowLoading(true);
         setNotFound(false);
         const fetchUserData = async () => {
-
             try {
                 const response = await fetch(config.apiUrl + `user/user-information/${id}`, {
                     headers: config.headers
@@ -56,21 +56,18 @@ export const UserPage = () => {
                     setAge(data.age);
                     setDescription(data.description);
                     setCourse(data.course);
-                    console.log("successful fetching of userdata");
+                    console.log("Successful fetching of userdata");
                     setNotFound(false);
                     setLoading(false);
-
                 } else {
-                    console.log(new Error("Failed to fetch Userdata"));
+                    console.log(new Error("Failed to fetch userdata"));
                     setNotFound(true);
                     setLoading(false);
-
                 }
             } catch (error) {
-                console.error("Error fetching Data:", error);
+                console.error("Error fetching data:", error);
                 setNotFound(true);
                 setLoading(false);
-
             }
         };
 
@@ -81,7 +78,7 @@ export const UserPage = () => {
             setPostsNotFound(false);
 
             try {
-                const response = await fetch(config.apiUrl + `post/user-posts/${userId}`, {
+                const response = await fetch(config.apiUrl + `post/user-posts/${id}`, {
                     headers: headersWithJwt
                 });
                 if (response.ok) {
@@ -89,172 +86,173 @@ export const UserPage = () => {
                     if (data.length === 0) {
                         setPostsNotFound(true);
                     } else {
-                        console.log(("Successful fetching of user posts"));
+                        console.log("Successful fetching of user posts");
                         setUserPosts(data);
                         setPostsLoading(false);
                         setPostsNotFound(false);
                     }
-
                 } else {
-                    console.log(new Error("Failed to fetch User Posts"));
+                    console.log(new Error("Failed to fetch user posts"));
                     setPostsLoading(false);
                     setPostsNotFound(true);
-
                 }
             } catch (error) {
-                console.error("Error when retrieving the search data:", error)
+                console.error("Error when retrieving the search data:", error);
                 setPostsLoading(false);
                 setPostsNotFound(true);
-                // handle error
             }
         };
 
         fetchUserPosts();
 
-
-    }, [id, userId, config.headers, config.apiUrl]);
-
-
-    const handleFollow = async (following:boolean) => {
-        if (userId === null) return;
-        isFollowing();
-        if (following) {
+        const checkIfFollowing = async () => {
             try {
+                const response = await fetch(config.apiUrl + `friendship/is-following-user`, {
+                    method: 'POST',
+                    headers: headersWithJwt,
+                    body: JSON.stringify({
+                        requesterId: senderId,
+                        receiverId: id
+                    })
+                });
 
+                if (response.ok) {
+                    const isFollowing = await response.json();
+                    setFollow(isFollowing);
+                    setFollowLoading(false);
+                    console.log("Successfully checked following status");
+                } else {
+                    setFollow(false);
+                    setFollowLoading(false);
+                    console.error("Failed to check following status");
+                }
+            } catch (error) {
+                setFollow(false);
+                setFollowLoading(false);
+                console.error("Failed to check following status: ", error);
+            }
+        };
+        checkIfFollowing();
+    }, [id, senderId]);
+
+    const handleFollow = async (following: boolean) => {
+        if (userId === null) return;
+        if (!following) {
+            try {
                 const response = await fetch(config.apiUrl + `friendship/follow-user`, {
                     method: 'POST',
                     headers: headersWithJwt,
                     body: JSON.stringify({
-                        "requesterId": senderId,
-                        "receiverId": id
+                        requesterId: senderId,
+                        receiverId: id
                     })
                 });
 
                 if (response.ok) {
-                    if (following) {
-                        console.log("Successfully followed the user");
-                        setAmountFollower(prev => (prev !== null ? prev + 1 : 1));
-                    } else {
-                        console.log("Successfully unfollowed the user");
-                    }
-
+                    console.log("Successfully followed the user");
+                    setAmountFollower(prev => (prev !== null ? prev + 1 : 1));
+                    setFollow(true);
                 } else {
-                    if (following) {
-                        console.error("Failed to follow the user");
-                    } else {
-                        console.error("Failed to unfollow the user");
-                    }
-
+                    console.error("Failed to follow the user");
                 }
             } catch (error) {
-                if (following) {
-                    console.error("Failed to follow the user " + error);
-                } else {
-                    console.error("Failed to unfollow the user " + error);
-                }
+                console.error("Failed to follow the user: ", error);
             }
-        }
-        else {
+        } else {
             try {
-
                 const response = await fetch(config.apiUrl + `friendship/unfollow-user`, {
                     method: 'POST',
                     headers: headersWithJwt,
                     body: JSON.stringify({
-                        "requesterId": senderId,
-                        "receiverId": id
+                        requesterId: senderId,
+                        receiverId: id
                     })
                 });
 
                 if (response.ok) {
-                        console.log("Successfully unfollowed the user");
+                    console.log("Successfully unfollowed the user");
+                    setAmountFollower(prev => (prev !== null ? prev - 1 : 0));
+                    setFollow(false);
                 } else {
-                        console.error("Failed to unfollow the user");
+                    console.error("Failed to unfollow the user");
                 }
             } catch (error) {
-                console.error("Failed to unfollow the user " + error);
+                console.error("Failed to unfollow the user: ", error);
             }
         }
-    }
+    };
 
-
-    if (notFound) {
-        return (
-            <div className="page">
-                <Header/>
-                <div className="user-not-found">
-                    <h1 className="error">This User doesn´t exist</h1>
-                </div>
-                <Footer/>
-            </div>
-        );
-    }
-    if (loading) {
-        return (
-            <div className="page">
-                <Header/>
-                <div className="user-component">
-                    <div className="animation">
-                        <div className="loading-animation">
-                            <Lottie animationData={animationData}/>
-                        </div>
+    const FollowButton = () => {
+        if (userId === senderId) {
+            return <div></div>;
+        } else if (followLoading) {
+            return (
+                <div className="animation">
+                    <div className="loading-animation">
+                        <Lottie animationData={animationData}/>
                     </div>
                 </div>
-                <Footer/>
-            </div>
-        );
-    }
-
-
-    class FollowButton extends React.Component<{ userId: any, senderId: any }> {
-        render() {
-            let {userId, senderId} = this.props;
-            if (userId === senderId) {
-                return (
-                    <div></div>
-                );
-            } else if (follow) {
-                return (
-                    <button className="unfollow-button"  onClick={() => handleFollow(false)}>
-                        Unfollow
-                    </button>
-                );
-
-            }
+            );
+        } else if (follow) {
             return (
-                <button className="follow-button" onClick={() => handleFollow(true)}>
+                <button className="unfollow-button" onClick={() => handleFollow(true)}>
+                    Unfollow
+                </button>
+            );
+        } else {
+            return (
+                <button className="follow-button" onClick={() => handleFollow(false)}>
                     Follow
                 </button>
             );
         }
-    }
-    const isFollowing = async () => {
+    };
 
-        try {
-            const response = await fetch(config.apiUrl + `friendship/is-following-user`, {
-                method: 'POST',
-                headers: headersWithJwt,
-                body: JSON.stringify({
-                    "requesterId": senderId,
-                    "receiverId": id
-                })
-            });
-
-            if (response.ok) {
-                   setFollow(true);
-                    console.log("Successfully unfollowed the user");
-
-            } else {
-                setFollow(false);
-                    console.error("Failed to follow the user");
-
-            }
-        } catch (error) {
-                console.error("Failed to follow the user " + error);
-
+    const DisplayPosts = () => {
+        if (postsNotFound) {
+            return (
+                <div className="user-not-found">
+                    <h1 className="error">This User doesn´t have Posts</h1>
+                </div>
+            );
         }
-    }
-
+        if (postLoading) {
+            return (
+                <div className="user-post-component">
+                    <div className="animation">
+                        <div className="loading-animation">
+                            <Lottie animationData={animationData}/>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <div className="user-posts-header">Posts by {username}:</div>
+                    <div className="user-posts">
+                        <div className="search-results">
+                            {userPosts.map(post => (
+                                <UserPost
+                                    key={post.id}
+                                    id={post.id}
+                                    title={post.title}
+                                    description={post.description || ""}
+                                    tags={post.tags}
+                                    likeAmount={post.likeAmount}
+                                    commentAmount={post.commentAmount}
+                                    timestamp={post.timestamp}
+                                    postImage={post.postImage}
+                                    accountId={post.accountId}
+                                    username={post.username}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    };
 
     if (notFound) {
         return (
@@ -267,6 +265,7 @@ export const UserPage = () => {
             </div>
         );
     }
+
     if (loading) {
         return (
             <div className="page">
@@ -281,61 +280,6 @@ export const UserPage = () => {
                 <Footer/>
             </div>
         );
-    }
-
-
-    class DisplayPosts extends React.Component {
-
-        render() {
-            if (postsNotFound) {
-                return (
-
-                    <div className="user-not-found">
-                        <h1 className="error">This User doesn´t have Posts</h1>
-                    </div>
-                );
-            }
-            if (postLoading) {
-                return (
-
-                    <div className="user-post-component">
-                        <div className="animation">
-                            <div className="loading-animation">
-                                <Lottie animationData={animationData}/>
-                            </div>
-                        </div>
-                    </div>
-
-                );
-            }
-            else {
-                return (
-                    <div>
-                        <div className="user-posts-header">Posts by {username}:</div>
-                        <div className="user-posts">
-                            <div className="search-results">
-                                {userPosts.map(post => (
-                                    <UserPost
-                                        key={post.id}
-                                        id={post.id}
-                                        title={post.title}
-                                        description={post.description || ""}
-                                        tags={post.tags}
-                                        likeAmount={post.likeAmount}
-                                        commentAmount={post.commentAmount}
-                                        timestamp={post.timestamp}
-                                        postImage={post.postImage}
-                                        accountId={post.accountId}
-                                        username={post.username}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                );
-            }
-
-        }
     }
 
     return (
@@ -344,9 +288,9 @@ export const UserPage = () => {
             <div className="user-component">
                 <div className="user-page-picture">
                     {picture.id !== null && picture.imageData ? (
-                        <img className="user-image" alt={"user"} src={picture.imageData} loading="lazy"/>
+                        <img className="user-image" alt="user" src={picture.imageData} loading="lazy"/>
                     ) : (
-                        <img className="custom-image" alt={"random-image"} src={getDefaultOrRandomPicture(false)}/>
+                        <img className="custom-image" alt="random-image" src={getDefaultOrRandomPicture(false)}/>
                     )}
                 </div>
                 <div className="user-page-name">
@@ -355,31 +299,26 @@ export const UserPage = () => {
                 <div className="user-followers">
                     <span>Followers: {amountFollower}</span>
                 </div>
-                <div className="description-field">
-                    {course !== null ? (
+                {course !== null && (
+                    <div className="description-field">
                         <span>Course: {course}</span>
-                    ) : (
-                        <span>Course: Not set</span>
-                    )}
-                </div>
-                <div className="description-field">
-                    {age !== null ? (
+                    </div>)
+                }
+
+                {age !== null && (
+                    <div className="description-field">
                         <span>Age: {age}</span>
-                    ) : (
-                        <span>Age: Not set</span>
-                    )}
-                </div>
-                <div className="description-field">
-                    {description !== null ? (
-                        <span>Description: {description}</span>
-                    ) : (
-                        <span>Description: Not set</span>
-                    )}
-                </div>
-                <FollowButton userId={userId} senderId={senderId} />
+                    </div>
+                )}
+                {description !== null && (
+                    <div className="description-field">
+                        <span>{description}</span>
+                    </div>
+                )
+                }
 
+                <FollowButton/>
             </div>
-
             <div>
                 <div className="user-posts">
                     <DisplayPosts/>
@@ -388,5 +327,4 @@ export const UserPage = () => {
             <Footer/>
         </div>
     );
-
 };
